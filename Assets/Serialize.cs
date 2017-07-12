cbcae8c0ab664d40a54b51f91553d3adde9ac2b9
sinhaugoh <sinhaugoh.16@ichat.sp.edu.sg>
2017-07-12 18:38:45 +0800

    Completed

---
+++ Assets/Serialize.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public static class Serialize {
+    public static void SaveScore(UpdateScore curScore)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream stream = new FileStream(Application.persistentDataPath + "/score.swag", FileMode.Create);
+
+        ScoreData scoreData = new ScoreData(curScore);
+
+        bf.Serialize(stream, scoreData);
+        stream.Close();
+
+    }
+
+    public static int LoadScore()
+    {
+        if (File.Exists(Application.persistentDataPath + "/score.swag"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream stream = new FileStream(Application.persistentDataPath + "/score.swag", FileMode.Open);
+
+            ScoreData scoreData = bf.Deserialize(stream) as ScoreData;
+            stream.Close();
+            return scoreData.score;
+        }
+        else
+        {
+            Debug.Log("Error, no file exists");
+            return 0;
+        }
+    }	
+}
+
+[Serializable]
+public class ScoreData
+{
+    public int score;
+
+    public ScoreData(UpdateScore curScore)
+    {
+        score = curScore.score;
+    }
+}
---
+++ Assets/UpdateScore.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateScore : MonoBehaviour {
+    public Toggle toggle;
+    public Text scoreText;
+    public int score ;
+    public bool onVibration;
+    private int vibrationTrue;
+	// Use this for initialization
+	void Start () {
+        vibrationTrue = PlayerPrefs.GetInt("vibrationOn");
+        if(vibrationTrue==1)
+        {
+            toggle.isOn = true;
+        }
+        else
+        {
+            toggle.isOn = false;
+        }
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scoreText.text = "Score: " + score.ToString();
+        PlayerPrefs.SetInt("vibrationOn", vibrationTrue);
+        if (toggle.isOn)
+        {
+            onVibration = true;
+            vibrationTrue = 1;
+        
+        }
+        else
+        {
+            onVibration = false;
+            vibrationTrue = 0;
+        }
+        
+	}
+
+    public void Save()
+    {
+        Serialize.SaveScore(this);
+    }
+
+    public void Load()
+    {
+        int LoadedScore = Serialize.LoadScore();
+        score = LoadedScore;
+
+    }
+
+
+    public void Vibrate()
+    {
+        if(onVibration == true)
+        {
+            Handheld.Vibrate();
+        }
+        
+    }
+
+    public void Score(){
+        score++;
+
+    }
+}
